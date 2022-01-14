dca5f894ce6d0bc2f487e008171dbcd14c39a774
Soccertitan <tristan.nowacki@outlook.com>
2022-01-13 16:31:21 -0800

    Added FunctionLibrary and Ability Task for waiting on montage.

---
+++ Source/XIRemix/Public/FunctionLibrary/CombatFunctionLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CombatFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XIREMIX_API UCombatFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
